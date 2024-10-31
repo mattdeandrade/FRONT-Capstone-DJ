c7a173bf5954446e0757fc056b8d7401abefd4da
Blake <bpatin77@gmail.com>
2024-10-31 10:27:42 -0500

    Fixed Footer

--- FrontDJ/src/App.jsx
+++ FrontDJ/src/App.jsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
 import { TrackList } from "./features/tracks/TrackList.jsx";
-
+import { Footer } from "./layout/Footer.jsx";
 import "./App.css";
 import "./layout/table.css";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Router } from "react-router-dom";
 import Root from "./layout/Root.jsx";
 import Navbar from "./layout/Navbar.jsx";
-
 import { EditPage } from "./features/tracks/EditPage.jsx";
 import EditPlaylist from "./features/Playlist/EditPlaylist.jsx";
 import PlaylistList from "./features/Playlist/PlaylistList.jsx";
 import { SharePlaylist } from "./features/Playlist/SharePlaylist.jsx";
-
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
-
       // { path: "/playlists", element: <PlayLists /> },
 
       //  { path: "/login", element: <AuthForm /> },
@@ -27,20 +24,13 @@ export const router = createBrowserRouter([
       { path: "/tracks", element: <TrackList /> },
       { path: "/edit", element: <EditPage /> },
       { path: "/playlists", element: <PlaylistList /> },
-
     ],
   },
 ]);
 
 function App() {
   const navigate = useNavigate();
-  return (
-    <>
-      <Navbar></Navbar>
-      <TrackList></TrackList>
-      <Footer></Footer>
-    </>
-  );
+  return <></>
 }
 
 export default App;
