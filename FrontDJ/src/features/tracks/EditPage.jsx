4679091e475652f7a9842ba07911975914be1e9b
Matthew De Andrade <fayrfax@gmail.com>
2024-10-31 11:44:21 -0400

    style, edits

--- FrontDJ/src/features/tracks/EditPage.jsx
+++ FrontDJ/src/features/tracks/EditPage.jsx
@@ -1,17 +1,59 @@
 import { useState } from "react";
 export function EditPage() {
-  const [value, setValue] = useState(100);
-
+  const [pitchValue, setPitchValue] = useState(100);
+  const [tempoValue, setTempoValue] = useState(100);
+  const [loopA, setLoopA] = useState(100);
+  const [loopB, setLoopB] = useState(100);
   return (
-    <input
-      type="range"
-      min="0"
-      max="200"
-      value={value}
-      step="20"
-      onChange={(event) => {
-        setValue(event.target.value);
-      }}
-    />
+    <>
+      <div className="box-editpage">
+        <h2>Pitch</h2>
+
+        <input
+          type="range"
+          min="0"
+          max="200"
+          value={pitchValue}
+          step="20"
+          className="slider"
+          onChange={(event) => {
+            setPitchValue(event.target.value);
+          }}
+        />
+      </div>
+      <br></br>
+      <div className="box-editpage">
+        <h2>Tempo</h2>
+
+        <input
+          type="range"
+          min="0"
+          max="200"
+          value={tempoValue}
+          step="20"
+          className="slider"
+          onChange={(event) => {
+            setTempoValue(event.target.value);
+          }}
+        />
+      </div>
+      <br></br>
+      <div className="box-editpage">
+        <h2>Loop</h2>
+        <table className="loop">
+          <tbody>
+            <tr></tr>
+            <tr>
+              <td>
+                <button className="box-loop">A</button>
+              </td>
+              <td>
+                <button className="box-loop">B</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
--- FrontDJ/src/layout/Root.jsx
+++ FrontDJ/src/layout/Root.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
-//import { Footer } from "./footer";
+import { Footer } from "./footer";
 
 /** Root layout component */
 function Root() {
@@ -8,6 +8,7 @@ function Root() {
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
