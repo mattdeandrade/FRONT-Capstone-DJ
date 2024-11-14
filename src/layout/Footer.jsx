9f37c9a0b696f09d02af1bbcd2f69c8ee59aebdc
Matthew De Andrade <fayrfax@gmail.com>
2024-11-14 14:33:33 -0500

    trying to fix Footer issues, v7

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,43 @@
+//Refer to the Notes at the bottom of page for explanations to the code
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Footer.css";
+
+export default function Footer() {
+  const navigate = useNavigate();
+  //capitalized F in footer
+  return (
+    <footer>
+      <div className="footContent">
+        <h2>DJStreams</h2>
+        <nav>
+          <ul className="footerLinks">
+            <li>
+              <button onClick={() => navigate("/help")}>Help</button>
+            </li>
+            <li>
+              <button onClick={() => navigate("/social-media")}>
+                Social Media
+              </button>
+            </li>
+            <li>
+              <button onClick={() => navigate("/support")}>Support</button>
+            </li>
+            <li>
+              <button onClick={() => navigate("/faqs")}>FAQs</button>
+            </li>
+            <li>
+              <button onClick={() => navigate("/about-us")}>About Us</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+}
+
+//-------------- Notes -----------------
+// Created an import for Link (Line 3) inorder to navigate to other pages (if necessary)
+// import css file(Line 4) in order to apply styling
+//created an unordered list of links (<nav> tag is used to wrap the list of links.)
+//each link is contained in a <li> element
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer.jsx";
 
 /** Root layout component */
 function Root() {
