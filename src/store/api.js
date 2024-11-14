222483ea882aa82e27324c8ca920bc311bed3bc5
Matthew De Andrade <fayrfax@gmail.com>
2024-11-14 13:57:17 -0500

    deployment

--- src/store/api.js
+++ src/store/api.js
@@ -7,7 +7,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
  */
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://back-capstone-dj.onrender.com",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       token && headers.set("authorization", `Bearer ${token}`);
